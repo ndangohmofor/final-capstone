c8849b04d6a76fb2b80c64ef7cf1a0bc4485db45
Yvette Johnson <yj4868146@gmail.com>
2022-04-05 16:44:01 -0400

    Edited CreateAccout pages

---
+++ src/main/java/com/techelevator/dao/JdbcAccountDao.java
@@ -0,0 +1,40 @@
+package com.techelevator.dao;
+
+        import com.techelevator.authentication.PasswordHasher;
+        import com.techelevator.model.Account;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.jdbc.core.JdbcTemplate;
+        import org.springframework.stereotype.Component;
+
+        import javax.sql.DataSource;
+
+@Component
+public class JdbcAccountDao implements AccountDao {
+
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcAccountDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+
+    }
+
+    @Override
+    public long createAccount(Account account) {
+        System.out.println("account object in dao is "+account);
+
+        long newId = jdbcTemplate.queryForObject(
+                "INSERT INTO user_profile(first_name,last_name,email,photo,goal,user_id) VALUES (?, ?, ?, ?,?,?) RETURNING id", Long.class,
+                account.getFirstName(), account.getLastName(), account.getEmail(),
+                account.getPhoto(), account.getGoal(), account.getUserId());
+
+        return newId;
+    }
+}
+
+
+
+
+
+
