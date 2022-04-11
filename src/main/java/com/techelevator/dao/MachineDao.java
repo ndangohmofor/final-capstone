6fec3b239ffc70d1030c73b51030a8577bc7f163
William Brooks <wil.brooks07@gmail.com>
2022-04-11 12:01:29 -0500

    Updated addMachine query

--- src/main/java/com/techelevator/dao/MachineDao.java
+++ src/main/java/com/techelevator/dao/MachineDao.java
@@ -9,6 +9,6 @@ public interface MachineDao {
     List<Machine> getMachinesByName(String machineName);
     List<Machine> getMachinesByType(String machineType);
     void updateMachineUsage();
-    void addMachine(String machineName, String machineType, String machineReference);
+    void addMachine(String machineName, String machineType, String machineReference, int machineUsage);
 
 }
