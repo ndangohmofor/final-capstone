224ff3897366e4db39feb59ca7613a89b3f324c7
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2022-04-12 16:00:10 -0400

    updated the db scripts

--- database/create-capstone-db.sh
+++ database/create-capstone-db.sh
@@ -2,12 +2,9 @@
 BASEDIR=$(dirname $0)
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres workout &&
-<<<<<<< HEAD
 psql -U postgres -d  workout -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d workout -f "$BASEDIR/user.sql" &&
 psql -U postgres -d workout -f "$BASEDIR/data.sql"
-=======
 psql -U postgres -d workout -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d workout -f "$BASEDIR/user.sql" &&
 psql -U postgres -d workout -f "$BASEDIR/data.sql"
->>>>>>> 9587b3ae3c08239ccc550d94e550a4a6a2c242da
